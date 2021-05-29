369ca56601a9577830e9392c1cfd6f33156724dc
Arkhipov Danil <anfobox@gmail.com>
2021-05-29 18:28:16 +0700

    Add NewField

--- 1Lab/In_Airplane.cpp
+++ 1Lab/In_Airplane.cpp
@@ -2,9 +2,10 @@
 
 Airplane* In_Airplane(ifstream& ifst) {
     Airplane* A = new Airplane; //Выделяем память под самолет
-    
+
     ifst >> A->Speed; //Считываем скорость
     ifst >> A->Distance; //Считывает расстояние м/у пунктами отправления и назначения
+    ifst >> A->Cargo_weight; //Считываем массу груза
     ifst >> A->Flight_range; //Считываем дальность полета
     ifst >> A->Load_capacity; //Считываем грузоподъемность
 
