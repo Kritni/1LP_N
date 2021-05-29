642da1c6aa0816ee5f49c86014c8bd864e21b649
Arkhipov Danil <anfobox@gmail.com>
2021-05-29 18:39:30 +0700

    Add NewField

---
+++ 1Lab/Airplane_Travel_time.cpp
@@ -0,0 +1,5 @@
+#include "Airplane_Travel_time.h"
+
+double Airplane_Travel_time(int Speed, int Distance) {
+	return (double)Distance / (double)Speed;
+}
---
+++ 1Lab/Airplane_Travel_time.h
@@ -0,0 +1,8 @@
+#ifndef AIRPLANE_TRAVEL_TIME_H
+#define AIRPLANE_TRAVEL_TIME_H
+
+#include "Airplane.h"
+
+double Airplane_Travel_time(int Speed, int Distance);
+
+#endif // AIRPLANE_TRAVEL_TIME_H
